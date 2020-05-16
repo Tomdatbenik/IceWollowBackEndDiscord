3be337dc44e63e10e8f9fdc94530be4fecad36b3
Tomdatbenik <tomvankaathoven@gmail.com>
2020-05-16 16:55:00 +0200

    GItignore

---
+++ UserComponent/src/main/java/usercomponent/controllers/UserController.java
@@ -0,0 +1,51 @@
+package usercomponent.controllers;
+
+import com.google.gson.Gson;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import usercomponent.models.User;
+import usercomponent.service.UserService;
+
+@CrossOrigin
+@RestController
+@AllArgsConstructor
+@RequestMapping(path= "user", produces = MediaType.APPLICATION_JSON_VALUE)
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(value = "test", consumes = "application/json", produces = "application/json")
+    public String testPost() {
+        return "test";
+    }
+
+    @GetMapping(value = "test")
+    public String testGet() {
+        Gson gson = new Gson();
+
+        return gson.toJson(new User());
+    }
+
+    @GetMapping(value = "getById/{userId}")
+    public User getUserById(@PathVariable("userId") int userId)
+    {
+        return userService.getUserById(userId);
+    }
+
+    @GetMapping(value = "getUserByEmail")
+    public User getUserByEmail(String email)
+    {
+        return userService.getUserByEmail(email);
+    }
+
+    @PostMapping(value = "/add")
+    public boolean addUser(@RequestBody String user)
+    {
+        Gson gson = new Gson();
+        return userService.addUser(gson.fromJson(user,User.class));
+    }
+
+}
